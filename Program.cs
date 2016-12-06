d0d37429f9d98ed66d5aef145d17804934a09b13
Jason Norris <jasonwellsnorris@gmail.com>
2016-12-06 14:19:59 -0600

    Created MonoGame project.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RicochetMono
+{
+  public static class Program
+  {
+    [STAThread]
+    static void Main()
+    {
+      using (RicochetGame game = new RicochetGame())
+      {
+        game.Run();
+      }
+    }
+  }
+}
---
+++ RicochetGame.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RicochetMono
+{
+  public class RicochetGame : Game
+  {
+    GraphicsDeviceManager m_GraphicsDeviceManager;
+    SpriteBatch m_SpriteBatch;
+
+    public RicochetGame()
+    {
+      m_GraphicsDeviceManager = new GraphicsDeviceManager(this);
+
+      Content.RootDirectory = "Content";
+    }
+
+    protected override void Initialize()
+    {
+      base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+      m_SpriteBatch = new SpriteBatch(GraphicsDevice);
+    }
+
+    protected override void UnloadContent()
+    {
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+      if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+      {
+        Exit();
+      }
+
+      base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+      GraphicsDevice.Clear(Color.CornflowerBlue);
+
+      base.Draw(gameTime);
+    }
+  }
+}
